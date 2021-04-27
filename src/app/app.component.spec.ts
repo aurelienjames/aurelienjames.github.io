ef013238562899c17a02cf5e46b082dba9ac22bd
Aurelien James <apf.james@gmail.com>
2021-04-27 11:24:52 +0200

    fixed unit test failing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('This website is under construction...');
+    expect(compiled.querySelector('h2').textContent).toContain('hello world');
   });
 });
