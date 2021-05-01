3bd75b975df40cc4afdc38d5d36bc9a8ce8e1bdf
Aurelien James <apf.james@gmail.com>
2021-05-01 11:23:22 +0200

    save state of project

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
     entry: './src/index.ts',
     devtool: 'inline-source-map',
@@ -18,7 +20,23 @@ module.exports = {
         extensions: [ '.tsx', '.ts', '.js' ],
     },
     output: {
+        publicPath: 'dist',
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{
+                from: '**/*.html',
+                context: './src',
+                force: true,
+            }
+            ],
+        })
+      ],
 };
