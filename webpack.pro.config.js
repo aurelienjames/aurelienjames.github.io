352afabb0ba07e371613ad06637ddfe690fd8134
Aurelien James <apf.james@gmail.com>
2021-05-13 11:03:28 +0200

    Setup webpack dev and prod config

---
+++ webpack.pro.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js' ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({ 
+            template: path.resolve(__dirname, './src/index.html'),
+        })
+      ],
+};
